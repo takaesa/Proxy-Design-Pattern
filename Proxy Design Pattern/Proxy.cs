f341e0bf8d5e77b8862fa08425ad509a877dbf83
Takae <93442902+takaesa@users.noreply.github.com>
2024-04-23 12:05:48 +0700

    Add project files.

---
+++ Proxy Design Pattern/Proxy.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy_Design_Pattern
+{
+    public class Proxy : ISubject
+    {
+        private RealSubject _realSubject;
+
+        public Proxy(RealSubject realSubject)
+        {
+            this._realSubject = realSubject;
+        }
+        public void Request()
+        {
+            if (this.CheckAccess())
+            {
+                this._realSubject.Request();
+
+                this.LogAccess();
+            }
+        }
+
+        public bool CheckAccess()
+        {
+            Console.WriteLine("Proxy: Checking access prior to firing a real request.");
+
+            return true;
+        }
+
+        public void LogAccess()
+        {
+            Console.WriteLine("Proxy: Logging the time of request.");
+        }
+    }
+}
